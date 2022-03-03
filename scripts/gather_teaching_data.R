82b4f4142f1eca31fa733919d0b7575264a1a609
poniitty <pekka.niittynen@helsinki.fi>
2022-03-03 09:41:54 +0200

    gather 2.0

--- scripts/gather_teaching_data.R
+++ scripts/gather_teaching_data.R
@@ -18,74 +18,11 @@ cl<-startMPIcluster()
 registerDoMPI(cl)
 
 foreach(i = shps, .packages = c("terra","tidyverse","sf","fasterize","tidyr")) %dopar% {
+# for(i in shps){
   # i <- "/scratch/project_2003061/tiilikka/mosaiced/planet_2020-9-26_09-10-00_0f4e.tif_trainpoints.shp"
   p <- st_read(i)
   
   if(nrow(p) > 2){
-    
-    p <- p %>% slice(-1) %>% 
-      mutate(X1 = as.numeric(substr(X1, 1, 1)))
-    
-    bind_cols(p,
-              fill(st_drop_geometry(p), X1, .direction = "down") %>% rename(X2 = X1),
-              fill(st_drop_geometry(p), X1, .direction = "up") %>% rename(X3 = X1)) %>% 
-      filter(X2 == X3) %>%
-      select(-X1, -X3) %>% 
-      rename(X1 = X2) -> p
-    
-    if(grepl(".tif_", i)){
-      r <- rast(gsub("_trainpoints.shp","",i))
-    } else {
-      r <- rast(gsub("_trainpoints.shp",".tif",i))
-    }
-    names(r) <- c("blue","green","red","infrared")
-
-    chm <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/chm.tif"))
-    chm <- project(chm, r)
-
-    chm_conif <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/chm_conif.tif"))
-    chm_conif <- project(chm_conif, r)
-
-    slope <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/slope.tif"))
-    slope <- project(slope, r)
-
-    pisr <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/pisr/",
-                        if(grepl(".tif_", i)){
-                          gsub("_trainpoints.shp","",tail(strsplit(i, "/")[[1]],1))
-                        } else {
-                          gsub("_trainpoints.shp",".tif",tail(strsplit(i, "/")[[1]],1))
-                        }))
-    pisr <- project(pisr, r)
-
-    r[["blue_min"]] <- focal(r[[1]], w = 5, fun = "min", expand = T)
-    r[["infrared_min"]] <- focal(r[[4]], w = 5, fun = "min", expand = T)
-
-    r[["blue_max"]] <- focal(r[[1]], w = 5, fun = "max", expand = T)
-    r[["infrared_max"]] <- focal(r[[4]], w = 5, fun = "max", expand = T)
-    
-    if(file.exists(gsub("_trainpoints.shp","_mask.shp",i))){
-      
-      msk <- st_read(gsub("_trainpoints.shp","_mask.shp",i))
-      
-      if(class(msk$geometry)[1] == "sfc_GEOMETRYCOLLECTION"){
-        clouds <- r[[1]]
-        clouds[] <- 0
-        names(clouds) <- "clouds"
-      } else {
-        msk <- msk %>% mutate(X1 = as.numeric(substr(X1, 1, 1))) %>% 
-          mutate(X1 = ifelse(is.na(X1), 2, X1))
-        
-        clouds <- fasterize(msk, raster(chm), field = "X1", fun = "max")
-        clouds <- rast(clouds)
-        names(clouds) <- "clouds"
-        clouds[is.na(clouds)] <- 0
-      }
-    }
-    
-    rs <- c(r, chm, chm_conif, slope, pisr, clouds)
-    
-    p <- p %>% mutate(X1 = as.numeric(substr(X1, 1, 1))) %>% 
-      filter(!is.na(X1))
     
     if(grepl(".tif_", i)){
       imageid <- gsub("_trainpoints.shp","",tail(strsplit(i, "/")[[1]],1))
@@ -93,12 +30,95 @@ foreach(i = shps, .packages = c("terra","tidyverse","sf","fasterize","tidyr")) %
       imageid <- gsub("_trainpoints.shp",".tif",tail(strsplit(i, "/")[[1]],1))
     }
     
-    p <- bind_cols(st_drop_geometry(p) %>% slice(st_coordinates(p) %>% as.data.frame() %>% pull(L1)),
-                   terra::extract(rs, st_coordinates(p) %>% as.data.frame() %>% select(X,Y)) %>% select(-ID)) %>% 
-      filter(!is.na(blue)) %>% 
-      mutate(imageid = imageid)
+    areaid <- rev(strsplit(i, "/")[[1]])[3]
     
-    write_csv(p, paste0("extracted/", imageid, ".csv"))
+    if(!file.exists(paste0("extracted/", imageid,"_", areaid, ".csv"))){
+      
+      p <- p %>% slice(-1) %>% 
+        mutate(X1 = as.numeric(substr(X1, 1, 1)))
+      
+      bind_cols(p,
+                fill(st_drop_geometry(p), X1, .direction = "down") %>% rename(X2 = X1),
+                fill(st_drop_geometry(p), X1, .direction = "up") %>% rename(X3 = X1)) %>% 
+        filter(X2 == X3) %>%
+        select(-X1, -X3) %>% 
+        rename(X1 = X2) -> p
+      
+      if(grepl(".tif_", i)){
+        r <- rast(gsub("_trainpoints.shp","",i))
+      } else {
+        r <- rast(gsub("_trainpoints.shp",".tif",i))
+      }
+      names(r) <- c("blue","green","red","nir")
+      
+      chm <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/chm.tif"))
+      chm <- project(chm, r)
+      
+      chm_conif <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/chm_conif.tif"))
+      chm_conif <- project(chm_conif, r)
+      
+      slope <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/slope.tif"))
+      slope <- project(slope, r)
+      
+      pisr <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/pisr/",
+                          if(grepl(".tif_", i)){
+                            gsub("_trainpoints.shp","",tail(strsplit(i, "/")[[1]],1))
+                          } else {
+                            gsub("_trainpoints.shp",".tif",tail(strsplit(i, "/")[[1]],1))
+                          }))
+      pisr <- project(pisr, r)
+      
+      r[["blue_min"]] <- focal(r[[1]], w = 5, fun = "min", expand = T)
+      r[["infrared_min"]] <- focal(r[[4]], w = 5, fun = "min", expand = T)
+      
+      r[["blue_max"]] <- focal(r[[1]], w = 5, fun = "max", expand = T)
+      r[["infrared_max"]] <- focal(r[[4]], w = 5, fun = "max", expand = T)
+      
+      if(file.exists(gsub("_trainpoints.shp","_mask.shp",i))){
+        
+        msk <- st_read(gsub("_trainpoints.shp","_mask.shp",i))
+        
+        if(class(msk$geometry)[1] == "sfc_GEOMETRYCOLLECTION"){
+          clouds <- r[[1]]
+          clouds[] <- 0
+          names(clouds) <- "clouds"
+        } else {
+          msk <- msk %>% mutate(X1 = as.numeric(substr(X1, 1, 1))) %>%
+            mutate(X1 = ifelse(is.na(X1), 2, X1))
+          
+          clouds <- fasterize(msk, raster(chm), field = "X1", fun = "max")
+          clouds <- rast(clouds)
+          names(clouds) <- "clouds"
+          clouds[is.na(clouds)] <- 0
+        }
+      }
+      
+      r_march <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/planet_march_median.tif"))
+      r_march <- project(r_march, r)
+      names(r_march) <- paste0("march_", c("blue","green","red","nir"))
+      
+      r_july <- rast(paste0(strsplit(i, "mosaiced")[[1]][1], "predictors/planet_july_median.tif"))
+      r_july <- project(r_july, r)
+      names(r_july) <- paste0("july_", c("blue","green","red","nir"))
+      
+      rs <- c(r, chm, chm_conif, slope, pisr, clouds, r_march, r_july)
+      
+      p <- p %>% mutate(X1 = as.numeric(substr(X1, 1, 1))) %>% 
+        filter(!is.na(X1))
+      
+      if("L1" %in% names(st_coordinates(p) %>% as.data.frame())){
+        p <- p %>% slice(st_coordinates(p) %>% as.data.frame() %>% pull(L1))
+      }
+      
+      p <- bind_cols(st_drop_geometry(p),
+                     terra::extract(rs, st_coordinates(p) %>% as.data.frame() %>% select(X,Y)) %>% select(-ID)) %>%
+        filter(!is.na(blue)) %>%
+        mutate(imageid = imageid)
+      
+      write_csv(p, paste0("extracted/", imageid,"_", areaid, ".csv"))
+      
+    }
+    
   }
 }
 
